ed65dead79731f29d0e6a7d48e69b355386742ef
Jonas Aklin <jonas.aklin@gmail.com>
2019-01-19 14:17:04 +0100

    project structure added

---
+++ ConflictCatalogServer/Base/RuntimeChecks/Argument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Base.RuntimeChecks
+{
+    public static class Argument
+    {
+        public static void AssertIsNotNull(object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+}

21da93ab0343c0db5a8994a0ae1e4edf7da4c927
Jonas Aklin <jonas.aklin@gmail.com>
2019-03-04 16:43:28 +0100

    DB updated, ImportedConflictProcessor improvements

--- ConflictCatalogServer/ConflictAndActorMapping/ImportedConflictProcessor.cs
+++ ConflictCatalogServer/ConflictAndActorMapping/ImportedConflictProcessor.cs
@@ -32,57 +32,6 @@ namespace ConflictAndActorMapping
         {
             this.CleanUpUnusedNations();
             this.ProcessImportedConflictsAndCreateNations();
-
-
-            ////Logger.Log.Info("Reading imported conflicts...");
-            ////var importedConflicts = this.readAllImportedConflictsRepository.ReadAll();
-            ////Logger.Log.Info($"{importedConflicts.Count} imported conflicts read.");
-
-            ////var conflicts = this.parser.ParseImportedConflicts(importedConflicts);
-            ////var actors = conflicts
-            ////    .SelectMany(c => c.Sides)
-            ////    .SelectMany(s => s.Actors)
-            ////    .ToList();
-
-            ////Logger.Log.Info($"All: {actors.Count}");
-
-            ////var uniqueActors = actors.Select(a => a.FullName).Distinct().ToList();
-            ////Logger.Log.Info($"Unique actors: {uniqueActors.Count}");
-
-            ////var uniqueLocations = actors
-            ////    .Select(a => a.Location)
-            ////    .Where(l => !int.TryParse(l, out int n))
-            ////    .Distinct().ToList();
-
-            ////Logger.Log.Info($"Unique locations: {uniqueLocations.Count}");
-            ////Logger.Log.Info("");
-
-            ////uniqueLocations.Sort();
-
-            ////foreach (var location in uniqueLocations)
-            ////{
-            ////    Logger.Log.Info(location);
-            ////}
-
-
-
-
-            ////foreach(var c in conflicts)
-            ////{
-            ////    Logger.Log.Info("----------------------------------------------");
-            ////    Logger.Log.Info("Summary: " + c.Summary);
-            ////    foreach (var s in c.Sides)
-            ////    {
-            ////        Logger.Log.Info("    >>>");
-
-            ////        foreach (var a in s.Actors)
-            ////        {
-            ////            Logger.Log.Info("    - " + a);
-            ////        }
-
-            ////        Logger.Log.Info("");
-            ////    }
-            ////}
         }
 
         private void CleanUpUnusedNations()
@@ -142,6 +91,8 @@ namespace ConflictAndActorMapping
             {
                 this.nationsRepository.Insert(newNation);
             }
+
+            Logger.Log.Info($"{newNations.Count} nations created.");
         }
     }
 }

347f617ea4e90b300eedb1b414d4f251e104e7ed
Jonas Aklin <jonas.aklin@gmail.com>
2019-03-04 17:30:28 +0100

    Web Server project added

---
+++ ConflictCatalogServer/ConflictWebServer/RequestHandler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ConflictWebServer
+{
+    public static class RequestHandler
+    {
+        public static Task HandleRequests(HttpContext context)
+        {
+            return context.Response.WriteAsync("Hello World!");
+        }
+    }
+}
---
+++ ConflictCatalogServer/ConflictWebServer/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConflictWebServer
+{
+    class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Run(context =>
+            {
+                return RequestHandler.HandleRequests(context);
+            });
+        }
+    }
+}

f47753cb8ce9675cdeae25a614c9aa503f2a61e3
Jonas Aklin <jonas.aklin@gmail.com>
2019-03-05 13:29:53 +0100

    "running" sketch

--- WebApp/sketch.js
+++ WebApp/sketch.js
@@ -1,16 +1,32 @@
+
+let sketchCanvas;
+let state;
+
 function setup()
 {
-    var parentDiv = document.getElementById('conflicts');
-    var width = parentDiv.offsetWidth;
-    var height = parentDiv.offsetHeight;
+    let parentDiv = document.getElementById('conflicts');
+    let width = parentDiv.offsetWidth;
+    let height = parentDiv.offsetHeight;
 
-    var sketchCanvas = createCanvas(width, height);
+    sketchCanvas = createCanvas(width, height);
     sketchCanvas.parent("conflicts");
     
+    state =
+    {
+        pos: 0
+    };
     // sketchCanvas.background(255, 100, 100);
 }
 
 function draw()
 {
-    background(255, 255, 100);
+    clear();
+
+    state.pos++;
+    // if (state.pos > 800)
+    // {
+    //     state.pos = 0;
+    // }
+
+    ellipse(state.pos, 900, 80, 80);
 }
